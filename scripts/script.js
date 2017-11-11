abba1764f465e8aa3ce16e25a2c1c0c154cce1f7
James Logue <logueja@gmail.com>
2017-11-11 13:22:59 -0700

    Link tone js and nexus ui

---
+++ scripts/script.js
@@ -0,0 +1,38 @@
+// var audioCtx = new (window.AudioContext || window.webkitAudioContext);
+
+// $('.start').on('click', function(){
+//   sinea.start();
+//   sineb.start();
+//   sinec.start();
+// })
+
+// $('.stop').on('click', function(){
+//   sinea.stop();
+//   sineb.stop();
+//   sinec.stop();
+// })
+
+// var volume = audioCtx.createGain();
+// volume.connect(audioCtx.destination);
+
+// var sinea = audioCtx.createOscillator();
+// sinea.frequency.value = 440;
+// sinea.type = 'sine';
+
+// sinea.connect(volume);
+
+// var sineb = audioCtx.createOscillator();
+// sineb.frequency.value = 523.25;
+// sineb.type = 'sine';
+// sineb.connect(volume);
+
+// var sinec = audioCtx.createOscillator();
+// sinec.frequency.value = 699;
+// sinec.type = 'square';
+// sinec.connect(volume)
+
+
+var dial = new Nexus.Dial('#dial');
+
+
+
